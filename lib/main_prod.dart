b109152b81a22dff87b4f7f403e4fe419ffafebb
byteVortex0 <mahmodahmed201559@gmail.com>
2025-10-29 05:50:10 +0300

    .

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:track_and_deploy/env_variables.dart';
 import 'package:track_and_deploy/track_and_deploy.dart';
 
 import 'firebase_options.dart';
@@ -9,8 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  await EnvVariables.instance.init(envType: EnvTypeEnum.prod);
 
   runApp(const MyApp());
 }
